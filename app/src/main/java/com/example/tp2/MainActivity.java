b21ca5c7d38e2c989390744d57247db1304d69e5
sirpepit0 <rewt2000@gmail.com>
2020-02-24 10:54:30 +0100

    exo 2 fait

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.strictmode.InstanceCountViolation;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Création écouteur
+        Ecouteur e = new Ecouteur();
+
+        //Récupération du composant bouton
+        Button bouton = (Button)findViewById(R.id.button);
+
+        //Enregistrement
+        bouton.setOnClickListener(e);
+    }
+
+    class Ecouteur implements View.OnClickListener {
+        //Récupération du TextView '...'
+        TextView texte = (TextView)findViewById(R.id.textThreeDots);
+        int i = 1;
+        @Override
+        public void onClick(View v) {
+            texte.setText("Clic #"+i);
+            i++;
+        }
     }
 }
