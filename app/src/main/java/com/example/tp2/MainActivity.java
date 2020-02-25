8b1139bdb37477c88d3a964344aa500ba485d340
sirpepit0 <rewt2000@gmail.com>
2020-02-25 12:07:05 +0100

    exo 3 fait

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.strictmode.InstanceCountViolation;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,22 +21,55 @@ public class MainActivity extends AppCompatActivity {
 
         // Création écouteur
         Ecouteur e = new Ecouteur();
+        Ecouteur eTexte = new Ecouteur();
 
         //Récupération du composant bouton
-        Button bouton = (Button)findViewById(R.id.button);
+        Button b1 = (Button)findViewById(R.id.buttonPlus);
+        Button b2 = (Button)findViewById(R.id.buttonMoins);
+        Button b3 = (Button)findViewById(R.id.buttonMultiplie);
+        Button b4 = (Button)findViewById(R.id.buttonDivise);
 
         //Enregistrement
-        bouton.setOnClickListener(e);
+        b1.setOnClickListener(e);
+        b2.setOnClickListener(e);
+        b3.setOnClickListener(e);
+        b4.setOnClickListener(e);
     }
 
     class Ecouteur implements View.OnClickListener {
         //Récupération du TextView '...'
-        TextView texte = (TextView)findViewById(R.id.textThreeDots);
-        int i = 1;
+        TextView texte = (TextView)findViewById(R.id.textResultat);
+
+        //Récupération des composants opérandes
+        EditText op1 = (EditText)findViewById(R.id.opérande1);
+        EditText op2 = (EditText)findViewById(R.id.opérande2);
+
+        int res = 0;
+
         @Override
         public void onClick(View v) {
-            texte.setText("Clic #"+i);
-            i++;
+            /* Méthode par switch : */
+
+            switch (v.getId()) {
+                case R.id.buttonPlus:
+                    res = Integer.parseInt(op1.getText().toString()) + Integer.parseInt(op2.getText().toString());
+                    texte.setText("" + res);
+                    break;
+                case R.id.buttonMoins:
+                    res = Integer.parseInt(op1.getText().toString()) - Integer.parseInt(op2.getText().toString());
+                    texte.setText("" + res);
+                    break;
+                case R.id.buttonMultiplie:
+                    res = Integer.parseInt(op1.getText().toString()) * Integer.parseInt(op2.getText().toString());
+                    texte.setText("" + res);
+                    break;
+                case R.id.buttonDivise:
+                    res = Integer.parseInt(op1.getText().toString()) / Integer.parseInt(op2.getText().toString());
+                    texte.setText("" + res);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + v);
+            }
         }
     }
 }
